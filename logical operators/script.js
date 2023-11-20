c1bfca9d66fa730b84aa595fa51a4d68293d10b0
Ruslan Korolev <RSKorolev@bk.ru>
2023-11-20 12:04:38 +0700

    Login verification

--- logical operators/script.js
+++ logical operators/script.js
@@ -7,5 +7,30 @@
 // let age = prompt('Введите число');
 // if (!(age >= 14 && age <= 90)) alert('Число не находится в диапаоне от 14 до 90');
 
-let age = prompt('Введите число');
-if (age < 14 || age > 90) alert('Число не находится в диапаоне от 14 до 90');
+// let age = prompt('Введите число');
+// if (age < 14 || age > 90) alert('Число не находится в диапаоне от 14 до 90');
+
+
+
+// https://learn.javascript.ru/task/check-login
+
+let password = 0;
+let user = prompt('Кто там?');
+if (user == null || user == '') {
+    alert('Отменено');
+}
+else if (user == !'') {
+    alert('Я вас не знею');
+} else if (user == 'Админ') {
+    password = prompt('Пароль?');
+    if (password == 'Я главный') {
+        alert('Здравствуйте!');
+    } else if (password == null || password == '') {
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+}
+else {
+    alert('Я вас не знею')
+}

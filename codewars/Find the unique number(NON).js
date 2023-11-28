7d647458665a5440d02cbf17fe4361e650ba0594
Ruslan Korolev <RSKorolev@bk.ru>
2023-11-28 11:17:33 +0700

    find the unique number not done

---
+++ codewars/Find the unique number(NON).js
@@ -0,0 +1,5 @@
+function booleanToString(b) {
+    let valueToBoolean = Boolean(b);
+    let booleanToString = String(valueToBoolean);
+    return booleanToString;
+}
--- codewars/Is it a palindrome.js
+++ codewars/Is it a palindrome.js
@@ -1,5 +1,9 @@
-function booleanToString(b) {
-    let valueToBoolean = Boolean(b);
-    let booleanToString = String(valueToBoolean);
-    return booleanToString;
+function isPalindrome(x) {
+    let arrA = x.toUpperCase().split('');
+    let arrB = x.toUpperCase().split('').reverse();
+    for (let i = 0; i < arrA.length; i++) {
+        if (arrA[i] !== arrB[i]) {
+            return false;
+        }
+    } return true;
 }

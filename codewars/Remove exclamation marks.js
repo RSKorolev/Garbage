c568dd521de1a5c9bc41ba2db367ea2e97536ad7
Ruslan Korolev <RSKorolev@bk.ru>
2023-12-01 12:32:04 +0700

    added for loop

---
+++ codewars/Remove exclamation marks.js
@@ -0,0 +1,15 @@
+function removeExclamationMarks(s) {
+    let newStr = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != '!') {
+            newStr += s[i];
+        };
+    } return newStr
+}
+
+console.log(removeExclamationMarks('!ghbd')); // 'ghbd'
+
+
+
+
+

1dc6db775d3be1582149cbd70083da78f960bbda
Ruslan Korolev <RSKorolev@bk.ru>
2023-12-01 13:11:19 +0700

    modified task

--- codewars/Abbreviate a Two Word Name.js
+++ codewars/Abbreviate a Two Word Name.js
@@ -2,4 +2,4 @@ function abbrevName(name) {
     let nameToArray = name.toUpperCase().split(' ');
     return `${nameToArray[0][0]}.${nameToArray[1][0]}`
 };
-console.log(abbrevName('ruslan kor')); //'R.K'
+console.log(abbrevName('ruslan kor'));   //'R.K'

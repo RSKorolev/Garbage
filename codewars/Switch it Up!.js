43cbcb59cfe9d4b7d24a314ae3629c2179ad782a
Ruslan Korolev <RSKorolev@bk.ru>
2023-12-04 17:57:50 +0700

    modified task

--- codewars/Switch it Up!.js
+++ codewars/Switch it Up!.js
@@ -1,7 +1,16 @@
-function betterThanAverage(classPoints, yourPoints) {
-    let sum = 0;
-    for (let i = 0; i < classPoints.length; i++)
-        sum += classPoints[i];
-    let averageSum = sum / classPoints.length;
-    return averageSum < yourPoints;
+function switchItUp(number) {
+    switch (number) {
+        case 0: return 'Zero';
+        case 1: return 'One';
+        case 2: return 'Two';
+        case 3: return 'Three';
+        case 4: return 'Four';
+        case 5: return 'Five';
+        case 6: return 'Six';
+        case 7: return 'Seven';
+        case 8: return 'Eight';
+        case 9: return 'Nine';
+    }
 }
+
+console.log(switchItUp(9)); //'Nine'
